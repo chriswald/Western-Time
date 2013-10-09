a86fcc401d740d25f6920c947799d14a2b801b44
Chris Wald <wald.j.chris@gmail.com>
2013-10-09 00:40:29 +0000

    Simplified transition animations.

--- js/mobile/menu.js
+++ js/mobile/menu.js
@@ -1,29 +1,13 @@
 $(document).ready(function() {
-    var menu_exposed = false;
-    
     if (screen.width <= 320) {
         $(".icon").width("32px");
         $(".icon").height("32px");
     }
     
-    $("#menu_body").height(0);
     $("#menu_body").addClass("hidden");
     
     $("#menu_button").click(function(){
-        
-        if (menu_exposed) {
-            $("#menu_body").animate({
-                height: 0
-            }, {done: function(){$("#menu_body").addClass("hidden");}});
-            menu_exposed = false;
-        }
-        else {
-            $("#menu_body").animate({
-                height: 500,
-                scrollTo: 0
-            }, {start: function(){$("#menu_body").removeClass("hidden");}});
-            menu_exposed = true;
-        }
+        $("#menu_body").slideToggle();
     });
     
     $("#program_select").change(function() {
@@ -34,7 +18,4 @@ $(document).ready(function() {
     $("input[name='college']").change(function() {
         populateProgramSelect();
     });
-    
-    $("#menu_body").height($(window).height()-$("#menu_bar").height());
-    $("#menu_body").css("top", -$("#menu_body").height());
 });
--- js/mobile/searchopts.js
+++ js/mobile/searchopts.js
@@ -3,35 +3,27 @@ var timeout = 200;
 
 $(document).ready(function() {
     var search_exposed = false;
-    $("#search_body").height(0);
     $("#search_body").addClass("hidden");
     
     $("#search_button").click(function() {
         if (search_exposed) {
-            $("#search_body").animate({
-                height: 0
-            }, {done: function(){$("#search_body").addClass("hidden");}});
+            $("#search_body").slideUp();
             search_exposed = false;
             $(document).focus();
         }
         else {
-            $("#search_body").animate({
-                height: 90,
-                scrollTo: 0
-            }, {start: function(){$("#search_body").removeClass("hidden");}});
+            $("#search_body").slideDown();
             search_exposed = true;
             $("#search_bar").focus();
         }
     });
     
-    var search_opts = {empty_func: emptySearch, done_searching: doneSearching};
-    
     $("#search_bar").on("input", function() {
         clearTimeout(typingTimer);
+        var search_opts = {empty_func: emptySearch, done_searching: doneSearching};
         typingTimer = setTimeout(function() {Search(search_opts);}, timeout);
     });
     
-    $("#search_body").css("top", -$("#search_body").height());
 });
 
 function emptySearch() {
