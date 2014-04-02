0aa95896abfc6046f38a14c59faac99fae30ad24
Chris Wald <wald.j.chris@gmail.com>
2014-04-02 01:42:45 +0000

    Changed search behaviour.

--- js/searchopts.js
+++ js/searchopts.js
@@ -30,27 +30,26 @@ $(document).ready(function() {
         clearTimeout(typingTimer);
         typingTimer = setTimeout(function() {Search(search_opts);}, timeout);
     });
+    
+    $("#program").change(function() {
+        $("#search_bar").val("");
+    })
+    
+    $("input:radio[name=college]").change(function() {
+        $("#search_bar").val("");
+    })
 });
 
 // Called if the search box is empty. Re-enables the program dropdown
 // and college radio buttons, and repopulates the section table.
 function emptySearch() {
-    $("#program").removeAttr("disabled");
-    $("#all").removeAttr("disabled");
-    $("#ems").removeAttr("disabled");
-    $("#lae").removeAttr("disabled");
-    $("#bilsa").removeAttr("disabled");
     getProgramSections();
 }
 
 // Called if the search box is not empty. Diables the program
 // dropdown and college radio buttons.
 function fullSearch() {
-    $("#program").attr("disabled", "disabled");
-    $("#all").attr("disabled", "disabled");
-    $("#ems").attr("disabled", "disabled");
-    $("#lae").attr("disabled", "disabled");
-    $("#bilsa").attr("disabled", "disabled");
+    
 }
 
 // Always called at the end of the search. Populates the section
